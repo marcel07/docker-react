e5b2f434c6953a36d25e48083cfd55f9f2a32e96
marcel07 <marcelchristie@gmail.com>
2021-10-15 00:49:08 +0200

    WIP

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Newest Text
+          This has changed
         </p>
         <a
           className="App-link"
